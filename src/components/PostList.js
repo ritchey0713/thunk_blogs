8bf611ddf981d3036ec4206db03cd0bf1abbe96d
Dj <ritchey0713@gmail.com>
2019-12-27 12:15:33 -0500

    add comp class

---
+++ src/components/PostList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class PostList extends Component {
+  render(){
+    return (
+      <div>
+        List
+      </div>
+    )
+  }
+}
+
+export default PostList
