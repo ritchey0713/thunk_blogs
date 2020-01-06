1ebe4cc98359e5dda4b1b03e4829ae5b644fa0f0
Dj <ritchey0713@gmail.com>
2020-01-06 10:36:07 -0500

    add comp to render

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { fetchPosts } from "../actions/index"
+import UserHeader from './UserHeader'
 
 class PostList extends Component {
 
@@ -18,6 +19,7 @@ class PostList extends Component {
               <h2>{post.title}</h2>
               <p>{post.body}</p>
             </div>
+            <UserHeader userId={post.userId} />
           </div>
         </div>
       )
