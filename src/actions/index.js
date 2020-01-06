12a5051c0faae45027d2949d955d9007b4e2e28e
Dj <ritchey0713@gmail.com>
2020-01-06 10:35:52 -0500

    add action

--- src/actions/index.js
+++ src/actions/index.js
@@ -19,3 +19,10 @@ export const fetchPosts = () => async (dispatch) => {
 //     }
 //   }
 // }
+export const fetchUser = (id) => async (dispatch) => {
+  const resp = await jsonPlaceHolder.get(`/users/${id}`)
+  dispatch({
+    type: "FETCH_USER",
+    payload: resp.data
+  })
+}
